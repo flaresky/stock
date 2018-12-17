a7114f169341a4bfa560ef65917c42aa2a118546
flaresky <flaresky@gmail.com>
2018-12-17 15:12:00 +0800

    s

---
+++ new_stock.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import re
+import datetime
+import utils
+import traceback
+
+ds_ptn = re.compile(r".*(\d{2})-(\d{2}).*")
+def transform_date(ds):
+    res = ds_ptn.search(ds)
+    if res is not None:
+        month = res.group(1)
+        day = res.group(2)
+        today = datetime.datetime.today()
+        if month >= today.month:
+            return "%s-%s-%s"%(today.year, month, day)
+        else:
+            return "%s-%s-%s"%(today.year+1, month, day)
+    return None
+
+def main():
+    today = datetime.datetime.today()
+    ts = today.strftime("%Y-%m-%d")
+#     ts = "2018-12-18"
+    stocks = []
+    url = "http://www.jisilu.cn/jisiludata/newstock.php?qtype=apply"
+    jo = utils.fetch_json(url)
+    for row in jo['rows']:
+        cell = row['cell']
+        name = row['id']
+        sid = cell['stock_cd']
+        apply_dt = transform_date(cell['apply_dt'])
+        if apply_dt == ts:
+            stocks.append("%s-%s"%(sid, name))
+    
+    if len(stocks) > 0:
+        ns = "申购新股：%s"%(' '.join([f.encode('utf-8') for f in stocks]))
+        es = EmailSender()
+        es.send_mail("申购新股", ns)
+        es.close()
+        utils.print_with_time(ns.decode('utf-8'))
+    utils.print_with_time("Done")
+
+if __name__ == '__main__':
+    main()
